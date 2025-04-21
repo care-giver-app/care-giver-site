812ea48cd8513c17f61a85d1263c480f570ed451
Trevor <tew4fs@virginia.edu>
2025-04-21 13:44:37 -0500

    make care path default

--- apps/care-giver-site/src/app/app.routes.ts
+++ apps/care-giver-site/src/app/app.routes.ts
@@ -1,14 +1,8 @@
 import { Routes } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 export const appRoutes: Routes = [
     {
         path: '',
-        component: NxWelcomeComponent,
-        pathMatch: 'full',
-    },
-    {
-        path: 'care',
         loadComponent: () =>
             import('@care-giver-site/care').then((m) => m.CareComponent),
     },
