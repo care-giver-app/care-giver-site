b894e02f5e0afb673130cd342f24d8b912dd38f5
Trevor <twilliams0095@gmail.com>
2026-04-26 11:58:03 -0500

    fix(navbar,receiver-selection): address code review quality issues

--- libs/care/src/lib/care/navbar/navbar.component.ts
+++ libs/care/src/lib/care/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { AuthService } from '@care-giver-site/services';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,7 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'care-navbar',
-  imports: [CommonModule, MatMenuModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, RouterModule, MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
@@ -15,21 +16,12 @@ export class NavbarComponent {
   @Output() menuToggle = new EventEmitter<void>();
 
   private authService = inject(AuthService);
-  userName = '';
-
-  constructor() {
-    this.authService.getUserFirstName().then(name => {
-      this.userName = name;
-    });
-  }
 
   toggleMenu() {
     this.menuToggle.emit();
   }
 
   signOut() {
-    this.authService.signOutUser().then(() => {
-      window.location.reload();
-    });
+    this.authService.signOutUser();
   }
 }
