fb2718a64a724d4a283f9c21da74559c1c3dd5c8
Trevor <twilliams0095@gmail.com>
2026-04-27 20:19:23 -0500

    feat(routing): nest authenticated routes under ShellComponent

--- apps/care-giver-site/src/app/app.routes.ts
+++ apps/care-giver-site/src/app/app.routes.ts
@@ -7,24 +7,29 @@ export const appRoutes: Routes = [
         path: '',
         canActivate: [AuthGuard],
         loadComponent: () =>
-            import('@care-giver-site/care').then((m) => m.DashboardComponent),
-    },
-    {
-        path: 'stats',
-        canActivate: [AuthGuard],
-        loadComponent: () =>
-            import('@care-giver-site/care').then((m) => m.StatsComponent),
+            import('@care-giver-site/care').then((m) => m.ShellComponent),
+        children: [
+            {
+                path: '',
+                loadComponent: () =>
+                    import('@care-giver-site/care').then((m) => m.DashboardComponent),
+            },
+            {
+                path: 'stats',
+                loadComponent: () =>
+                    import('@care-giver-site/care').then((m) => m.StatsComponent),
+            },
+            {
+                path: 'submit-feedback',
+                loadComponent: () =>
+                    import('@care-giver-site/care').then((m) => m.FeedbackComponent),
+            },
+        ],
     },
     {
         path: 'auth',
         loadComponent: () =>
             import('@care-giver-site/auth').then((m) => m.AuthComponent),
-    },
-    {
-        path: 'submit-feedback',
-        canActivate: [AuthGuard],
-        loadComponent: () =>
-            import('@care-giver-site/care').then((m) => m.FeedbackComponent),
     },
     {
         path: 'mobile-download',
